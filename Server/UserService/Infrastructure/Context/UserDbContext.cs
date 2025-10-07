bba37e45472f0b0e81851246c3e9bf9bd21ee859
Dima Rodoh <rodohlebov.dima@gmail.com>
2025-10-08 00:51:12 +0700

    Setting up Relationships

--- Server/UserService/Infrastructure/Context/UserDbContext.cs
+++ Server/UserService/Infrastructure/Context/UserDbContext.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore;
 using UserSerivce.Core.Entities;
 
@@ -11,6 +12,33 @@ namespace UserService.Infrastructure.Context
             DbContextOptions<UserDbContext> options) : base(options)
         {
             Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(20);
+                entity.Property(u => u.LastName)
+                    .HasMaxLength(30);
+                entity.Property(u => u.MiddleName)
+                    .HasMaxLength(20);
+                entity.Property(u => u.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30);
+                entity.Property(u => u.BirthDate);
+                entity.Property(u => u.PasswordHash)
+                    .IsRequired();
+                entity.Property(u => u.CreatedAt)
+                    .IsRequired();
+                entity.Property(u => u.AvatarPath)
+                    .IsRequired();
+                entity.Property(u => u.PhoneNumber)
+                    .IsRequired();
+            });
         }
     }
 }
