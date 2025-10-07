6e961efd7099488edf22333c6cccbe6f95b4b25c
Dima Rodoh <rodohlebov.dima@gmail.com>
2025-10-08 00:16:20 +0700

    add user entity

---
+++ Server/UserService/Core/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+namespace UserSerivce.Core.Entities;
+
+public record UserEntity(
+    Guid Id,
+    string UserName,
+    string FirstName,
+    string? LastName,
+    string? MiddleName,
+    string PhoneNumber,
+    string AvatarPath,
+    DateOnly? BirthDate,
+    DateTime CreatedAt,
+    string PasswordHash
+);
